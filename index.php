b3f795525b2b634341cd161abe3d0277e0e48d37
steveinc911 <stevesales@gmail.com>
2015-09-07 20:48:01 +0530

    installed packages needed

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="node_modules/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet/less" type="text/css" href="css/style.less">
+<script type="text/javascript">
+	less = { env: 'development' };
+</script>
+<script src="js/less.min.js"></script>
+<script src="node_modules/angular/angular.min.js"></script>
+<script src="node_modules/html5shiv/dist/html5shiv.js"></script>
+<title>The Zumba</title>
+</head>
+<body>
+<hgroup>
+<h1>The Zumba</h1>
+<h2>Coming soon...</h2>
+</hgroup>
+
+</body>
+</html>
